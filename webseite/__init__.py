b165390bae5c6e461a30f560e5bb7f0d147e3b41
andre <andreas.borowczak@gmail.om>
2022-04-10 12:50:47 +0200

    flask-admin hinzugefügt

--- webseite/__init__.py
+++ webseite/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from webseite.config import Config
-
+from flask_admin import Admin
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -12,6 +12,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
+admin = Admin()
 
 
 def create_app(config_class=Config):
@@ -22,12 +23,22 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    admin.init_app(app)
 
+    # flask-admin Anfang
+    from webseite.models import User, Post
+    from flask_admin.contrib.sqla import ModelView
+    admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Post, db.session))
+    # flask-admin Ende
+
+    # Blueprints Anfang
     from webseite.users.routes import users
     from webseite.posts.routes import posts
     from webseite.main.routes import main
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    # Blueprints Ende
 
     return app
